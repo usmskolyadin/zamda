ae608cdda6b49cecd1a49303360ca7c90ebb0f3a
usmskolyadin <koladinmaksim246@gmail.com>
2025-09-01 22:04:29 +0300

    fixes

--- frontend/src/widgets/ad-slider/index.tsx
+++ frontend/src/widgets/ad-slider/index.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 import { Advertisement } from "@/src/entities/advertisment/model/types";
 
