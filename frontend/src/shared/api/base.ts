12ef6286dbf721ebd14242a60a1e28ee298067de
usmskolyadin <koladinmaksim246@gmail.com>
2025-09-10 22:05:40 +0300

    collectstatic & api url

--- frontend/src/shared/api/base.ts
+++ frontend/src/shared/api/base.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://zamda.com';
 
 export async function apiFetch<T>(endpoint: string, options: RequestInit = {}): Promise<T> {
   const res = await fetch(`${API_URL}${endpoint}`, {
